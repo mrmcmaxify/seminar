399da0a563eb87b0d6a52729d22011178acaee22
Maxi <maxi.brenner@gmx.net>
2018-12-16 14:12:16 +0100

    +navbar + bootstrap +db-Anbindung

---
+++ application/views/templates/header.php
@@ -0,0 +1,27 @@
+<html>
+    <head>
+        <title>Seminarplatzvergabe</title>
+
+        <link rel = "stylesheet" type = "text/css" href = "<?php echo base_url(); ?>css/bootstrap.css">
+
+        <script type = 'text/javascript' src = "<?php echo base_url(); ?>js/bootstrap.js"></script>
+
+    </head>
+    <body>
+
+<!-- Navbar -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="<?php echo base_url(); ?>">Seminarplatzvergabe</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav">
+      <a class="nav-item nav-link active" href="<?php echo base_url(); ?>">Home <span class="sr-only">(current)</span></a>
+      <a class="nav-item nav-link" href="<?php echo base_url(); ?>about">About</a>
+    
+    </div>
+  </div>
+</nav>
+
+<div class="container">
