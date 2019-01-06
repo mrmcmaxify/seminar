47fc02ffa720586f9eeda0a54af78ad6fa081101
Maxi <maxi.brenner@gmx.net>
2019-01-06 19:06:21 +0100

    beginn arbeit zuweisen

--- application/models/Student_model.php
+++ application/models/Student_model.php
@@ -3,24 +3,27 @@
         public function __construct(){
             $this->load->database();
         }
-        
+        //Liest Bachelor-Studenten ohne Seminarplatz aus
         public function get_ba_ohne(){
             
-            $this->db->order_by('Name', 'ASC');
+            $this->db->order_by('Fachsemester', 'DESC');
+            $this->db->order_by('ECTS', 'DESC');
             $this->db->where('#Annahmen', '0');
             $this->db->where('BA/MA', 'BA');
             $query = $this->db->get('student');
             return $query->result_array();
         }
-
+        //Liest Bachelor-Studenten ohne Seminarplatz aus
         public function get_ma_ohne(){
             
-            $this->db->order_by('Name', 'ASC');
+            $this->db->order_by('Fachsemester', 'DESC');
+            $this->db->order_by('ECTS', 'DESC');
             $this->db->where('#Annahmen', '0');
             $this->db->where('BA/MA', 'MA');
             $query = $this->db->get('student');
             return $query->result_array();
         }
 
+       
         
     }
