0fc06f6af0a7de514d836793cfd5712fd02413c9
PelzP <44982447+PelzP@users.noreply.github.com>
2019-01-11 15:41:38 +0100

    Test

--- application/models/Seminar_model.php
+++ application/models/Seminar_model.php
@@ -8,4 +8,43 @@
             $query = $this->db->get('seminar');
             return $query->result_array();
         }
+
+        public function get_seminare_beworben($email){
+            $this->db->select('*');
+            $this->db->from('seminarbewerbung');
+            $this->db->join('seminar', 'seminarbewerbung.SeminarID = seminar.SeminarID', 'inner');
+            $this->db->where('E-Mail', $email);
+            $this->db->order_by('Seminarname', 'DESC');
+            $query = $this->db->get();
+            return $query->result_array();
+
+
+        }
+
+        // Liest seminarbewerbungen ein
+        public function bewerbung_hinzufuegen($MSNotwendig, $seminarid){
+    
+            if ($MSNotwendig === 1){
+                //User data array(seminarbewerbung)
+                $data = array(
+                    'e-mail' => $this->input->post('e-mail'),
+                    'seminarid' => $seminarid,
+                    'ms' => $this->input->post('ms')              
+                );
+            }
+
+            else{
+                $data = array(
+                    'e-mail' => $this->input->post('e-mail'),
+                    'seminarid' => $seminarid
+                );
+            } 
+
+
+            //insert seminarbewerbung(seminarbewerbung)
+            return $this->db->insert('seminarbewerbung', $data);
+
+            
+
+        }
     }
