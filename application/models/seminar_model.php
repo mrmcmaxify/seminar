72c377014d2e937aa95bcbe3486e6e4ba0e1c4f7
PelzP <44982447+PelzP@users.noreply.github.com>
2019-01-11 15:38:21 +0100

    bewerbung einfügen, motivationsschreiben überprüfen2

--- application/models/seminar_model.php
+++ application/models/seminar_model.php
@@ -3,30 +3,12 @@
         public function __construct(){
             $this->load->database();
         }
-        //Gibt alle Einträge der Seminar-Tabelle aus
+        
         public function get_seminare(){
             $query = $this->db->get('seminar');
             return $query->result_array();
         }
-        //Gibt Informationen zu bestimmtem Seminar aus, für Detailansicht
-        public function get_seminar($id){
-            $this->db->where('SeminarID', $id);
-            $query = $this->db->get('seminar');
-            return $query->result_array();
-        }
-        //gibt alle Bachelor-Seminare aus
-        public function get_seminare_ba(){
-            $this->db->where('BA/MA', 'BA');
-            $query = $this->db->get('seminar');
-            return $query->result_array();
-        }
-        //Gibt alle Master-Seminare aus
-        public function get_seminare_ma(){
-            $this->db->where('BA/MA', 'MA');
-            $query = $this->db->get('seminar');
-            return $query->result_array();
-        }
-        //Gibt alle Seminare aus, auf die sich ein bestimmter Student beworben hat
+
         public function get_seminare_beworben($email){
             $this->db->select('*');
             $this->db->from('seminarbewerbung');
@@ -36,6 +18,33 @@
             $query = $this->db->get();
             return $query->result_array();
 
+
+        }
+
+        // Liest seminarbewerbungen ein
+        public function bewerbung_hinzufuegen($MSNotwendig, $seminarid){
+    
+            if ($MSNotwendig === 1){
+                //User data array(seminarbewerbung)
+                $data = array(
+                    'e-mail' => $this->input->post('e-mail'),
+                    'seminarid' => $seminarid,
+                    'ms' => $this->input->post('ms')              
+                );
+            }
+
+            else{
+                $data = array(
+                    'e-mail' => $this->input->post('e-mail'),
+                    'seminarid' => $seminarid
+                );
+            } 
+
+
+            //insert seminarbewerbung(seminarbewerbung)
+            return $this->db->insert('seminarbewerbung', $data);
+
+            
 
         }
     }
