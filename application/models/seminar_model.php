98e3ae6f2c1669af0e13d200737190100f155aa9
jacky011 <ruppjacqueline@gmx.de>
2019-01-12 10:17:31 +0100

    git probl

--- application/models/seminar_model.php
+++ application/models/seminar_model.php
@@ -8,34 +8,4 @@
             $query = $this->db->get('seminar');
             return $query->result_array();
         }
-
-        public function get_seminar($id){
-            $this->db->where('SeminarID', $id);
-            $query = $this->db->get('seminar');
-            return $query->result_array();
-        }
-
-        public function get_seminare_ba(){
-            $this->db->where('BA/MA', 'BA');
-            $query = $this->db->get('seminar');
-            return $query->result_array();
-        }
-
-        public function get_seminare_ma(){
-            $this->db->where('BA/MA', 'MA');
-            $query = $this->db->get('seminar');
-            return $query->result_array();
-        }
-
-        public function get_seminare_beworben($email){
-            $this->db->select('*');
-            $this->db->from('seminarbewerbung');
-            $this->db->join('seminar', 'seminarbewerbung.SeminarID = seminar.SeminarID', 'inner');
-            $this->db->where('E-Mail', $email);
-            $this->db->order_by('Seminarname', 'DESC');
-            $query = $this->db->get();
-            return $query->result_array();
-
-
-        }
     }
