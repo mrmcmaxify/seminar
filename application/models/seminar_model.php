5ec7d7e4cba41f407c101e74c6e30117327a7658
PelzP <44982447+PelzP@users.noreply.github.com>
2019-01-19 17:37:30 +0100

    seminar zugesagt tabelle

--- application/models/seminar_model.php
+++ application/models/seminar_model.php
@@ -83,11 +83,10 @@
         }
 
         //Gibt alle Seminare aus, auf die sich ein bestimmter Student noch nicht beworben hat
-        public function get_seminare_not_beworben($email){
+        public function get_seminare_not_beworben($email, $bama){
             $this->db->select('*');
             $this->db->from('seminar');
-            $this->db->join('seminarbewerbung', 'seminar.SeminarID = seminarbewerbung.SeminarID', 'left');
-            $this->db->where_not_in('E-Mail', $email);
+            $this->db->where('seminar.BA/MA', $bama);
             $this->db->order_by('Seminarname', 'DESC');
             $query = $this->db->get();
 
@@ -148,10 +147,10 @@
          //gibt Seminare zurück, die vom Lehrstuhl zugesagt worden sind
          public function get_seminare_zugesagt($email, $bama){
             $this->db->select('*');
-            $this->db->from('seminarbewerbung');
-            $this->db->join('seminar', 'seminarbewerbung.SeminarID = seminar.SeminarID', 'inner');
+            $this->db->from('seminarzuteilung');
+            $this->db->join('seminar', 'seminarzuteilung.SeminarID = seminar.SeminarID', 'inner');
             $this->db->where('E-Mail', $email);
-            $this->db->where('Eingeladen', 1);
+            $this->db->where('seminar.BA/MA', $bama);
             $this->db->order_by('Seminarname', 'DESC');
             $query = $this->db->get();
             return $query->result_array();
