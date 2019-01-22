8f169140855bf80ebc4146e60082e9c62a113986
jacky011 <ruppjacqueline@gmx.de>
2019-01-22 15:51:00 +0100

    merge

---
+++ application/views/admin/inhaber_zu_hoch.php
@@ -0,0 +1,5 @@
+<?php echo "Pro Lehrstuhl oder Dekanat darf nur ein Inhaber angelegt werden. Das Anlegen eines weiteren Inhabers ist daher nicht mehr möglich." ?>
+</br>
+</br>
+</br>
+<a href="<?php echo base_url(); ?>admin/add_user" class="btn btn-primary" role="button">Ok</a>
