42043f31a0646c2a409da54c6d83f6e273e78d24
PelzP <44982447+PelzP@users.noreply.github.com>
2019-01-22 12:49:49 +0100

    Startseite mit richtigen Seminaren

--- application/controllers/Dekan.php
+++ application/controllers/Dekan.php
@@ -187,7 +187,7 @@
 
 		else{
 			$this->load->view('templates/header');
-			$this->load->view('pages/ausserhalb_frist_student');
+			$this->load->view('pages/ausserhalb_frist_dekan');
 			$this->load->view('templates/footer');
 		}
 	}
@@ -491,7 +491,7 @@
             
             if ( $heute < $startdatum) {
                 $this->load->view('templates/header');
-                $this->load->view('pages/ausserhalb_frist_student');
+                $this->load->view('pages/ausserhalb_frist_dekan');
                 $this->load->view('templates/footer');
 			} 
 			
@@ -529,12 +529,12 @@
             $bis = $this->Fristen_model->get_frist_ende($fristname);
             $frist_ende = $bis['0'];
             $enddatum = $frist_ende['Bis'];
-            $heute = '2019-02-13';
+            $heute = '2019-01-13';
 
             
             if ( $heute < $startdatum) {
                 $this->load->view('templates/header');
-                $this->load->view('pages/ausserhalb_frist_student');
+                $this->load->view('pages/ausserhalb_frist_dekan');
                 $this->load->view('templates/footer');
 			} 
 			
@@ -578,7 +578,7 @@
             
             if ( $heute < $startdatum) {
                 $this->load->view('templates/header');
-                $this->load->view('pages/ausserhalb_frist_student');
+                $this->load->view('pages/ausserhalb_frist_dekan');
                 $this->load->view('templates/footer');
 			} 
 			
