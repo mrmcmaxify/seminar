a83dbd019a31062cc04b64560cffde941459d2d7
PelzP <44982447+PelzP@users.noreply.github.com>
2019-02-02 13:07:45 +0100

    Update User_model.php

--- application/models/User_model.php
+++ application/models/User_model.php
@@ -16,6 +16,7 @@
                 'e-mail' => $this->input->post('e-mail'),
                 'vorname' => $this->input->post('vorname'),
                 'name' => $this->input->post('name'),
+                'studiengang' => $this->input->post('studiengang'),
                 'fachsemester' => $this->input->post('fachsemester'),
                 'ba/ma' => $this->input->post('ba/ma'),
                 'ects' => $this->input->post('ects'),
@@ -24,6 +25,7 @@
 
             //insert user(benutzeraccount)
             $this->db->insert('benutzeraccount', $data);
+            //insert user(student)
             return $this->db->insert('student', $data1);
 
            
@@ -164,5 +166,12 @@
 
             public function delete_user_dekan($email){
                 $this->db->where('E-Mail', $email)->delete('dekanat');
+            }
+
+            public function get_studiengang(){
+                $query = $this->db->get('studiengang');
+                return $query->result_array();
+
+
             }
 }
