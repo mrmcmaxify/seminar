649666ce2b5227c56e4f3e7dd888189d68cea574
jacky011 <ruppjacqueline@gmx.de>
2019-02-06 11:09:38 +0100

    Update seminaranlegen_model.php

--- application/models/seminaranlegen_model.php
+++ application/models/seminaranlegen_model.php
@@ -1,6 +1,6 @@
 <?php  
     class Seminaranlegen_model extends CI_Model{
-        public function seminaranlegen($lehrstuhlname){
+        public function seminaranlegen(){
            
             
                
@@ -8,7 +8,7 @@
              //User data array(seminar)
              $data1 = array(
                 'seminarname' => $this->input->post('seminarname'),
-                'lehrstuhlname' => $lehrstuhlname,
+                'lehrstuhlname' => $this->input->post('lehrstuhlname'),
                 'beschreibung' => $this->input->post('beschreibung'), 
                 'soll-teilnehmerzahl' => $this->input->post('soll-teilnehmerzahl'),
                 'semester' => $this->input->post('semester'),
@@ -38,7 +38,7 @@
             $query=$this->db->get();
             return $query->result_array();
          }
-         // Liefert den Startzeitpunkt eines Semesters
+         // Liefert das ausgewählte Seminar
          public function get_semesteranfang($bezeichnung){
             $this->db->select('anfang');
             $this->db->from('semesterzeiten');
